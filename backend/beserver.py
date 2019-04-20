7c5ce3f6ccde49ec3c9e8215d4fcab92c3675b9e
SVz <903943711@qq.com>
2019-04-20 22:32:39 +0800

    be change return code

--- backend/beserver.py
+++ backend/beserver.py
@@ -1,4 +1,6 @@
 import flask
+import sys
+sys.path.append(".")
 from flask import request, jsonify
 from werkzeug.datastructures import FileStorage
 from PIL import Image
@@ -19,7 +21,7 @@ def get_plate():
         plate = get_plate_from_model(img)
         return jsonify({"errno": 0, "errmsg": "ok", "plate": plate})
     except:
-        return jsonify({"errno": -1, "errmsg": "error", "plate": ""})
+        return jsonify({"errno": 1, "errmsg": "error", "plate": ""})
 
 
 app.run("", 8002)
